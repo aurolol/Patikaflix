b65f325c6024bf39e65acf0eaf6d75db0cda50c4
auro <auro@DESKTOP-AV8I9BT>
2024-09-05 21:32:16 +0300

    Add project files.

---
+++ Patikaflix/Comedy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patikaflix
+{
+    internal class Comedy
+    {
+        public string SeriesName { get; set; }
+
+        public string Type { get; set; }
+
+        public string Director { get; set; }
+
+    }
+}
---
+++ Patikaflix/Program.cs
@@ -0,0 +1,81 @@
+﻿using Patikaflix;
+using System.Runtime.CompilerServices;
+
+List<TVSeries> tVSeriesList = new List<TVSeries>();
+
+while (true)
+{
+    TVSeries tVSeries = new TVSeries()
+    {
+        SeriesName = GetStringInput("Lütfen dizi adını giriniz: "),
+
+        MadeYear = GetIntInput("Lütfen yapım yılını giriniz: "),
+
+        PublicationYear = GetIntInput("Dizinin başlama yılını giriniz: "),
+
+        Type = GetStringInput("Dizinin türünü giriniz: "),
+
+        Directors = GetStringInput("Lütfen yönetmenin adını giriniz: "),
+
+        Platform = GetStringInput("Dizi hangi platfomrda yayınlanıyordu: "),
+    };
+    tVSeriesList.Add(tVSeries);
+
+    Console.Write("Listeye başka bir dizi eklemek istiyor musunuz?(E/H): ");
+
+    string choose = Console.ReadLine().ToLower();
+
+    if (choose == "h")
+        break;
+
+}
+
+List<Comedy> comedylist = tVSeriesList
+                          .Where(c => c.Type.ToLower() == "komedi")
+                          .Select(c => new Comedy
+                          {
+                              SeriesName = c.SeriesName,
+                              Type = c.Type,
+                              Director = c.Directors,
+
+                          }).ToList();
+
+var sorted = comedylist.OrderBy(c => c.SeriesName)
+                       .ThenBy(c => c.Director)
+                       .ToList();
+
+Console.WriteLine("\n↓ Komedi Türünde Oluşturulan Diziler ↓\n");
+
+foreach (var comedy in sorted)
+{
+    Console.WriteLine($"Dizinin adı: {comedy.SeriesName}\nDizinin türü: {comedy.Type}\nDizinin yönetmeni: {comedy.Director}");
+}
+
+string GetStringInput(string text)
+{
+    Console.Write(text);
+    return Console.ReadLine();
+}
+
+int GetIntInput(string text)
+{
+    bool isValidInput = false;
+    int value = 0;
+    while (!isValidInput)
+    {
+        Console.Write(text);
+
+        string userInput = Console.ReadLine();
+        if (int.TryParse(userInput, out value))
+        {
+            isValidInput = true;
+        }
+        else
+        {
+            Console.WriteLine("Geçersiz işlem yaptınız. Lütfen sayı değeri giriniz");
+
+        }
+
+    }
+    return value;
+}
